7c481fc4301731fb8c9a6268bfd49f350a9c2319
Megan Middleton <uxtechnologist@gmail.com>
2024-07-26 11:54:42 -0700

    Connect to API data and update current temp and city name based on search

--- src/index.js
+++ src/index.js
@@ -1,60 +1,52 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
-/*
-// get a city from the user's input
-let city = prompt("Enter a city?");
+// select elements on the page that display weather information
+let cityDisplay = document.getElementById("city-name");
+let currentTempDisplay = document.getElementById("current-temp");
+let currentHumidityDisplay = document.getElementById("current-humidity");
 
-while (city === undefined || city.length === 0) {
-  city = prompt("Enter a city?");
+// select form for the input data
+let searchForm = document.querySelector("form");
+
+// data for API call for current weather data
+let apiKey = "0t46o23ed0fd5ba55b5549905c06fafe";
+let citySearched = "Lisbon";
+
+/**
+ * Parses data from the api response, then calls showData to display it
+ * @param {} response response from API call to get current weather data
+ */
+function handleApiData(response) {
+  let dataObj = response.data;
+  let cityName = dataObj.city;
+  let currentTemp = Math.round(dataObj.temperature.current);
+  let currentHumidity = Math.round(dataObj.temperature.humidity);
+  showData(cityName, currentTemp, currentHumidity);
 }
 
-// standardize by making it lower case and removing excess spaces
-city = city.trim().toLowerCase();
-
-if (weather[city] !== undefined) {
-  // if there's data for this city, return it
-
-  // first get the data to return
-  let tempCelsius = Math.round(weather[city].temp);
-  let tempFahrenheit = Math.round((weather[city].temp * 9) / 5 + 32);
-
-  // next form the message to return using the city's data
-  let infoMessage = `It is currently ${tempCelsius}°C (${tempFahrenheit}°F) in ${city} with a humidity of ${Math.round(
-    weather[city].humidity
-  )}%`;
-
-  alert(infoMessage);
-} else {
-  // if we don't have data for this city, then still reply to handle this case
-  let cityURL = city.replace(" ", "+"); // create a url friendly name for the city (if there are spaces in it)
-  let errorMessage = `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${cityURL}`;
-
-  alert(errorMessage);
+function showData(cityName, currentTemp, currentHumidity) {
+  cityDisplay.textContent = cityName;
+  currentTempDisplay.textContent = currentTemp;
+  currentHumidityDisplay.textContent = currentHumidity;
 }
-*/
 
-// functionality to update the date the display the current day and time
+searchForm.addEventListener("submit", (event) => {
+  // prevent reloading the page
+  event.preventDefault();
 
+  // get data from the form
+  const formData = new FormData(searchForm);
+  let queryEntered = formData.get("search");
+
+  // Note: currently not validating if they searched for a city we have data for
+  citySearched = queryEntered;
+
+  // construct api with new search terms
+  let apiUrlCurrentTemp = `https://api.shecodes.io/weather/v1/current?query=${citySearched}&key=${apiKey}&units=metric`;
+
+  // call API with new search term and update display
+  axios.get(apiUrlCurrentTemp).then(handleApiData);
+});
+
+// code to show an updated date/time
 // getDate() returns the updated date as a string
 function getDate() {
   let now = new Date();
@@ -88,29 +80,7 @@ function updateDate() {
   let currentDate = document.querySelector("#current-date");
   let updatedDate = getDate();
 
-  currentDate.innerHTML = updatedDate;
+  currentDate.textContent = updatedDate;
 }
 
 updateDate();
-
-// functionality for search form
-
-// getSearchInput() returns the input that the user entered into the city search form
-function getSearchInput() {
-  let searchInput = document.querySelector("#search-input").value;
-  return searchInput;
-}
-
-// updates the page title with the city that was searched for
-function updateCity(event) {
-  event.preventDefault();
-  let searchInput = getSearchInput();
-  let title = document.querySelector("h1");
-  title.innerHTML = searchInput;
-
-  // also update the current time displayed
-  updateDate();
-}
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", updateCity);
